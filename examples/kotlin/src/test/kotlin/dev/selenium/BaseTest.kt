1ea3481d80e80d6d045027653e1138eed648893b
ShankarShedge <156658082+ShankarShedge@users.noreply.github.com>
2024-01-15 18:18:49 +0530

    Update BaseTest.kt

--- examples/kotlin/src/test/kotlin/dev/selenium/BaseTest.kt
+++ examples/kotlin/src/test/kotlin/dev/selenium/BaseTest.kt
@@ -1,23 +1,31 @@
 package dev.selenium;
 
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterEach; 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance 
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-open class BaseTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) 
+
+open class BaseTest { 
+    
     lateinit var driver: WebDriver
     
     @BeforeEach
+    
     fun setup() {
+        
         driver = ChromeDriver()
     }
 
     @AfterEach
+    
     fun teardown() {
+        
         driver.quit()
+        
     }
+    
 }
